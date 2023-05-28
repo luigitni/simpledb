b4967146004603409ca52ebc3685221278884fff
Luigi Tanzini <tanzini.luigi@gmail.com>
2023-05-28 23:03:22 +0100

    Add metadata catalog

---
+++ meta/table.go
@@ -0,0 +1,171 @@
+package meta
+
+import (
+	"fmt"
+	"io"
+
+	"github.com/luigitni/simpledb/record"
+	"github.com/luigitni/simpledb/tx"
+)
+
+// NameMaxLen is the maximum len of a field or table name
+const NameMaxLen = 16
+
+type TableManager struct {
+	// tcat is the catalog table for tables
+	tcat record.Layout
+	// fcat is the catalog table for fields
+	fcat record.Layout
+}
+
+func NewTableManager() *TableManager {
+	tcats := record.MakeSchema()
+	tcats.AddStringField("tblname", NameMaxLen)
+	tcats.AddIntField("slotsize")
+	tcat := record.NewLayout(tcats)
+
+	fcats := record.MakeSchema()
+	fcats.AddStringField("tblname", NameMaxLen)
+	fcats.AddStringField("fldname", NameMaxLen)
+	fcats.AddIntField("type")
+	fcats.AddIntField("length")
+	fcats.AddIntField("offset")
+	fcat := record.NewLayout(fcats)
+
+	return &TableManager{
+		tcat: tcat,
+		fcat: fcat,
+	}
+}
+
+func (tm TableManager) Init(trans tx.Transaction) {
+	tm.CreateTable("tblcat", tm.tcat.Schema(), trans)
+	tm.CreateTable("fldcat", tm.tcat.Schema(), trans)
+}
+
+func (tm TableManager) CreateTable(tblname string, sch record.Schema, tr tx.Transaction) error {
+	layout := record.NewLayout(sch)
+
+	tcat := record.NewTableScan(tr, "tblcat", tm.tcat)
+	tcat.Insert()
+	if err := tcat.SetString("tblname", tblname); err != nil {
+		return err
+	}
+
+	if err := tcat.SetInt("slotsize", layout.SlotSize()); err != nil {
+		return err
+	}
+
+	tcat.Close()
+
+	// for each schema field, insert a record into the field catalog
+	fcat := record.NewTableScan(tr, "fldcat", tm.fcat)
+	for _, fname := range sch.Fields() {
+		fcat.Insert()
+		if err := fcat.SetString("tblname", tblname); err != nil {
+			return err
+		}
+
+		if err := fcat.SetString("fldname", fname); err != nil {
+			return err
+		}
+
+		if err := fcat.SetInt("type", int(sch.Type(fname))); err != nil {
+			return err
+		}
+
+		if err := fcat.SetInt("length", sch.Length(fname)); err != nil {
+			return err
+		}
+
+		if err := fcat.SetInt("offset", layout.Offset(fname)); err != nil {
+			return err
+		}
+	}
+	fcat.Close()
+	return nil
+}
+
+func (tm TableManager) Layout(tblname string, trans tx.Transaction) (record.Layout, error) {
+
+	var empty record.Layout
+
+	size := -1
+	tcat := record.NewTableScan(trans, "tblcat", tm.tcat)
+	for {
+		err := tcat.Next()
+		if err == io.EOF {
+			break
+		}
+
+		if err != nil {
+			return empty, err
+		}
+
+		tname, err := tcat.GetString("tblname")
+		if err != nil {
+			return empty, err
+		}
+
+		if tname == tblname {
+			size, err = tcat.GetInt("slotsize")
+			if err != nil {
+				return empty, err
+			}
+			break
+		}
+	}
+	tcat.Close()
+
+	if size < 0 {
+		// could not find the table in the catalogue
+		return empty, fmt.Errorf("could not find table %q in table catalog", tblname)
+	}
+
+	schema := record.MakeSchema()
+	offsets := map[string]int{}
+	fcat := record.NewTableScan(trans, "fldcat", tm.fcat)
+	for {
+		err := fcat.Next()
+		if err == io.EOF {
+			break
+		}
+
+		if err != nil {
+			return empty, err
+		}
+
+		tname, err := fcat.GetString("tblname")
+		if err != nil {
+			return empty, err
+		}
+
+		if tname == tblname {
+			fldname, err := fcat.GetString("fldname")
+			if err != nil {
+				return empty, err
+			}
+
+			fldtype, err := fcat.GetInt("type")
+			if err != nil {
+				return empty, err
+			}
+
+			fldlen, err := fcat.GetInt("length")
+			if err != nil {
+				return empty, err
+			}
+
+			offset, err := fcat.GetInt("offset")
+			if err != nil {
+				return empty, err
+			}
+
+			offsets[fldname] = offset
+			schema.AddField(fldname, record.FieldType(fldtype), fldlen)
+		}
+	}
+
+	fcat.Close()
+	return record.NewLayoutFromMetadata(schema, offsets, size), nil
+}
---
+++ meta/table_test.go
@@ -0,0 +1,47 @@
+package meta
+
+import (
+	"testing"
+
+	"github.com/luigitni/simpledb/record"
+	"github.com/luigitni/simpledb/test"
+	"github.com/luigitni/simpledb/tx"
+)
+
+func TestTableManager(t *testing.T) {
+
+	t.Cleanup(test.ClearTestFolder)
+
+	trans := tx.NewTx(test.MakeManagers())
+
+	tm := NewTableManager()
+	tm.Init(trans)
+
+	schema := record.MakeSchema()
+	schema.AddIntField("A")
+	schema.AddStringField("B", 9)
+
+	if err := tm.CreateTable("MyTable", schema, trans); err != nil {
+		t.Fatal(err)
+	}
+
+	layout, err := tm.Layout("MyTable", trans)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	t.Logf("MyTable has slot size %d", layout.SlotSize())
+
+	t.Log("MyTable fields:")
+	sch := layout.Schema()
+	for _, fname := range sch.Fields() {
+		switch sch.Type(fname) {
+		case record.INTEGER:
+			t.Logf("%s INTEGER", fname)
+		case record.STRING:
+			t.Logf("%s VARCHAR(%d)", fname, sch.Length(fname))
+		}
+	}
+
+	trans.Commit()
+}
