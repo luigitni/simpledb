f7917fed4c3b268ec2e3e3a46711cc5a517f0b36
Luigi Tanzini <tanzini.luigi@gmail.com>
2023-07-03 17:05:09 +0100

    Added predicate implementation

--- record/predicate.go
+++ record/predicate.go
@@ -1,5 +1,56 @@
 package record
 
+import "fmt"
+
+type Expression struct {
+	val   interface{}
+	fname string
+}
+
+func NewExpressionWithVal(v interface{}) Expression {
+	return Expression{val: v}
+}
+
+func NewExpressionWithField(fname string) Expression {
+	return Expression{fname: fname}
+}
+
+func (exp Expression) IsFieldName() bool {
+	return exp.fname != ""
+}
+
+func (exp Expression) AsConstant() interface{} {
+	return exp.val
+}
+
+func (exp Expression) AsFieldName() string {
+	return exp.fname
+}
+
+func (exp Expression) Evaluate(scan Scan) (interface{}, error) {
+	if exp.val != nil {
+		return exp.val, nil
+	}
+
+	return scan.GetVal(exp.fname)
+}
+
+func (exp Expression) AppliesTo(schema Schema) bool {
+	if exp.val != nil {
+		return true
+	}
+
+	return schema.HasField(exp.fname)
+}
+
+func (exp Expression) String() string {
+	if exp.val != nil {
+		return fmt.Sprintf("%v", exp.val)
+	}
+
+	return exp.fname
+}
+
 type Predicate struct{}
 
 func (p Predicate) IsSatisfied(s Scan) bool {
