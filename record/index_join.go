94304cc3a56eeadc2ccba96691c211401b6cfe2e
Luigi Tanzini <tanzini.luigi@gmail.com>
2024-05-26 16:37:24 +0100

    Implement index interfaces

---
+++ record/index_join.go
@@ -0,0 +1,157 @@
+package record
+
+import (
+	"io"
+
+	"github.com/luigitni/simpledb/file"
+)
+
+var _ Plan = &IndexJoinPlan{}
+
+type IndexJoinPlan struct {
+	firstPlan  Plan
+	secondPlan Plan
+	ii         IndexInfo
+	joinField  string
+	schema     Schema
+}
+
+func newIndexJoinPlan(firstPlan, secondPlan Plan, ii IndexInfo, joinField string) *IndexJoinPlan {
+	schema := NewSchema()
+	schema.AddAll(firstPlan.Schema())
+	schema.AddAll(secondPlan.Schema())
+
+	return &IndexJoinPlan{
+		firstPlan:  firstPlan,
+		secondPlan: secondPlan,
+		ii:         ii,
+		joinField:  joinField,
+		schema:     schema,
+	}
+}
+
+func (plan *IndexJoinPlan) Schema() Schema {
+	return plan.schema
+}
+
+func (plan *IndexJoinPlan) Open() Scan {
+	s := plan.firstPlan.Open()
+	ts := plan.secondPlan.Open().(*TableScan)
+	idx := plan.ii.Open()
+	return newIndexJoinScan(s, idx, plan.joinField, ts)
+}
+
+func (plan *IndexJoinPlan) BlocksAccessed() int {
+	return plan.firstPlan.BlocksAccessed() +
+		(plan.firstPlan.RecordsOutput() + plan.ii.BlocksAccessed()) +
+		plan.RecordsOutput()
+}
+
+func (plan *IndexJoinPlan) DistinctValues(fieldName string) int {
+	if plan.firstPlan.Schema().HasField(fieldName) {
+		return plan.firstPlan.DistinctValues(fieldName)
+	}
+
+	return plan.secondPlan.DistinctValues(fieldName)
+}
+
+func (plan *IndexJoinPlan) RecordsOutput() int {
+	return plan.firstPlan.RecordsOutput() * plan.ii.RecordsOutput()
+}
+
+var _ Scan = &IndexJoinScan{}
+
+type IndexJoinScan struct {
+	lhs       Scan
+	idx       Index
+	joinField string
+	rhs       *TableScan
+}
+
+func newIndexJoinScan(lhs Scan, idx Index, joinField string, rhs *TableScan) *IndexJoinScan {
+	ijs := &IndexJoinScan{
+		lhs:       lhs,
+		idx:       idx,
+		joinField: joinField,
+		rhs:       rhs,
+	}
+
+	ijs.BeforeFirst()
+	return ijs
+}
+
+func (ijs *IndexJoinScan) Close() {
+	ijs.lhs.Close()
+	ijs.idx.Close()
+	ijs.rhs.Close()
+}
+
+func (ijs *IndexJoinScan) BeforeFirst() {
+	ijs.lhs.BeforeFirst()
+	ijs.lhs.Next()
+	ijs.resetIndex()
+}
+
+func (ijs *IndexJoinScan) Next() error {
+	for {
+		err := ijs.idx.Next()
+
+		if err == nil {
+
+			rid, err := ijs.idx.DataRID()
+			if err != nil {
+				return err
+			}
+
+			ijs.rhs.MoveToRID(rid)
+			return nil
+		}
+
+		if err != io.EOF {
+			return err
+		}
+
+		if err := ijs.lhs.Next(); err != nil {
+			return err
+		}
+
+		ijs.resetIndex()
+	}
+}
+
+func (ijs *IndexJoinScan) resetIndex() error {
+	key, err := ijs.lhs.GetVal(ijs.joinField)
+	if err != nil {
+		return err
+	}
+
+	return ijs.idx.BeforeFirst(key)
+}
+
+func (ijs *IndexJoinScan) HasField(field string) bool {
+	return ijs.rhs.HasField(field) || ijs.lhs.HasField(field)
+}
+
+func (ijs *IndexJoinScan) GetVal(field string) (file.Value, error) {
+	if ijs.rhs.HasField(field) {
+		return ijs.rhs.GetVal(field)
+	}
+
+	return ijs.lhs.GetVal(field)
+}
+
+func (ijs *IndexJoinScan) GetInt(field string) (int, error) {
+	if ijs.rhs.HasField(field) {
+		return ijs.rhs.GetInt(field)
+	}
+
+	return ijs.lhs.GetInt(field)
+}
+
+func (ijs *IndexJoinScan) GetString(field string) (string, error) {
+	if ijs.rhs.HasField(field) {
+		return ijs.rhs.GetString(field)
+	}
+
+	return ijs.lhs.GetString(field)
+}
---
+++ record/index_update_planner.go
@@ -0,0 +1,213 @@
+package record
+
+import (
+	"io"
+
+	"github.com/luigitni/simpledb/file"
+	"github.com/luigitni/simpledb/sql"
+	"github.com/luigitni/simpledb/tx"
+)
+
+var _ UpdatePlanner = &IndexUpdatePlanner{}
+
+type IndexUpdatePlanner struct {
+	mdm *MetadataManager
+}
+
+func NewIndexUpdatePlanner(mdm *MetadataManager) *IndexUpdatePlanner {
+	return &IndexUpdatePlanner{
+		mdm: mdm,
+	}
+}
+
+func (planner *IndexUpdatePlanner) ExecuteInsert(data sql.InsertCommand, x tx.Transaction) (int, error) {
+	plan, err := NewTablePlan(x, data.TableName, planner.mdm)
+	if err != nil {
+		return 0, err
+	}
+
+	s := plan.Open().(UpdateScan)
+	defer s.Close()
+
+	if err := s.Insert(); err != nil {
+		return 0, err
+	}
+
+	rid := s.GetRID()
+
+	ii, err := planner.mdm.IndexInfo(x, data.TableName)
+	if err != nil {
+		return 0, err
+	}
+
+	for i, field := range data.Fields {
+		val := data.Values[i]
+		if err := s.SetVal(field, val); err != nil {
+			return 0, err
+		}
+
+		// check if the field is indexed. If it is, save it
+		info, ok := ii[field]
+		if !ok {
+			continue
+		}
+
+		idx := info.Open()
+		defer idx.Close()
+		if err := idx.Insert(val, rid); err != nil {
+			return 0, err
+		}
+	}
+
+	return 1, nil
+}
+
+func (planner *IndexUpdatePlanner) ExecuteModify(data sql.UpdateCommand, x tx.Transaction) (int, error) {
+	plan, err := NewTablePlan(x, data.TableName, planner.mdm)
+	if err != nil {
+		return 0, err
+	}
+
+	selectPlan := NewSelectPlan(plan, data.Predicate)
+	updateScan := selectPlan.Open().(UpdateScan)
+	defer updateScan.Close()
+
+	ii, err := planner.mdm.IndexInfo(x, data.TableName)
+	if err != nil {
+		return 0, err
+	}
+
+	rid := updateScan.GetRID()
+
+	updateIndex := func(oldVal file.Value, newVal file.Value) error {
+		info, ok := ii[data.Field]
+		if !ok {
+			return nil
+		}
+
+		idx := info.Open()
+		if err := idx.Delete(oldVal, rid); err != nil {
+			return err
+		}
+
+		if err := idx.Insert(newVal, rid); err != nil {
+			return err
+		}
+
+		return nil
+	}
+
+	c := 0
+
+	for {
+		err := updateScan.Next()
+		if err == io.EOF {
+			break
+		}
+
+		if err != nil {
+			return c, err
+		}
+
+		newVal, err := data.NewValue.Evaluate(updateScan)
+		if err != nil {
+			return c, err
+		}
+
+		oldVal, err := updateScan.GetVal(data.Field)
+		if err != nil {
+			return c, err
+		}
+
+		if err := updateScan.SetVal(data.Field, newVal); err != nil {
+			return c, err
+		}
+
+		if err := updateIndex(oldVal, newVal); err != nil {
+			return c, err
+		}
+
+		c++
+	}
+
+	return c, nil
+}
+
+func (planner *IndexUpdatePlanner) ExecuteDelete(data sql.DeleteCommand, x tx.Transaction) (int, error) {
+	plan, err := NewTablePlan(x, data.TableName, planner.mdm)
+	if err != nil {
+		return 0, err
+	}
+
+	selectPlan := NewSelectPlan(plan, data.Predicate)
+
+	ii, err := planner.mdm.IndexInfo(x, data.TableName)
+	if err != nil {
+		return 0, err
+	}
+
+	updateScan := selectPlan.Open().(UpdateScan)
+	defer updateScan.Close()
+
+	delFromIdx := func() error {
+		rid := updateScan.GetRID()
+		for field, info := range ii {
+			val, err := updateScan.GetVal(field)
+			if err != nil {
+				return err
+			}
+
+			idx := info.Open()
+			defer idx.Close()
+			if err := idx.Delete(val, rid); err != nil {
+				return err
+			}
+		}
+		return nil
+	}
+
+	c := 0
+	// first, delete the record rid from every index
+	for {
+		err := updateScan.Next()
+		if err == io.EOF {
+			break
+		}
+
+		if err != nil {
+			return c, err
+		}
+
+		if err := delFromIdx(); err != nil {
+			return c, err
+		}
+
+		if err := updateScan.Delete(); err != nil {
+			return c, err
+		}
+
+		c++
+	}
+
+	return c, nil
+}
+
+func (planner *IndexUpdatePlanner) ExecuteCreateIndex(data sql.CreateIndexCommand, x tx.Transaction) (int, error) {
+	if err := planner.mdm.CreateIndex(x, data.IndexName, data.TableName, data.TargetField); err != nil {
+		return 0, err
+	}
+
+	return 0, nil
+}
+
+func (planner *IndexUpdatePlanner) ExecuteCreateTable(data sql.CreateTableCommand, x tx.Transaction) (int, error) {
+	schema := NewSchema()
+	for _, f := range data.Fields {
+		schema.AddField(f.Name, f.Type, f.Len)
+	}
+	return 0, planner.mdm.CreateTable(data.TableName, schema, x)
+}
+
+func (planner *IndexUpdatePlanner) ExecuteCreateView(data sql.CreateViewCommand, x tx.Transaction) (int, error) {
+	return 0, planner.mdm.CreateView(data.ViewName, data.Definition(), x)
+}
